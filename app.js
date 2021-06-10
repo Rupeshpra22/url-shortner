68b9c0e7f66ff8932813651676a793608be3daae
Rupesh Prajapati <39094963+Rupeshpra22@users.noreply.github.com>
2021-06-10 23:09:27 +0530

    solved cors issue

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+let allowAccessOrigin = (req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+}
+
+app.use(allowAccessOrigin);
+
 app.get('/', (req,res)=>{
     res.send("URL Shortner backend");
 })
